86499b8030a10f8c5a5b67222a472465a1a9a96f
salmayassin <salmamyassinn@gmail.com>
2025-03-05 23:30:52 +0100

    Added test cases for normal items

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,42 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
 
+    // Normal Item
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void normalItem_qualityDecreasesSellinDecreases() {
+        Item[] items = { new Item("Normal Item", 11, 20) };
         GildedRose app = new GildedRose(items);
+        
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        
+        assertEquals(10, items[0].sellIn);
+        assertEquals(19, items[0].quality);
+    }
+
+    @Test
+    void normalItem_qualityDecreasesTwiceAsFastAfterSellIn() {
+        Item[] items = { new Item("Normal Item", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        
+        app.updateQuality();
+        
+        assertEquals(-1, items[0].sellIn);
+        assertEquals(18, items[0].quality); // Decreases by 2
+    }
+
+    @Test
+    void normalItem_qualityNeverGoesBelowZero() {
+        Item[] items = { new Item("Normal Item", 5, 0) };
+        GildedRose app = new GildedRose(items);
+        
+        app.updateQuality();
+        
+        assertEquals(4, items[0].sellIn);
+        assertEquals(0, items[0].quality);
     }
 
 }
