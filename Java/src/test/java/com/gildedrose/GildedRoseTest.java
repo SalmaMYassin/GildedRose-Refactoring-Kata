70d91acbd46ff0c6ef77c7984157558f2aff2fe4
salmayassin <salmamyassinn@gmail.com>
2025-03-05 23:38:35 +0100

    Adding test cases for Aged Brie

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -39,4 +39,38 @@ class GildedRoseTest {
         assertEquals(0, items[0].quality);
     }
 
+    // Aged Brie  
+    @Test
+    void agedBrie_qualityIncreasesSellinDecreases() {
+        Item[] items = { new Item("Aged Brie", 5, 10) };
+        GildedRose app = new GildedRose(items);
+        
+        app.updateQuality();
+        
+        assertEquals(4, items[0].sellIn);
+        assertEquals(11, items[0].quality);
+    }
+
+    @Test
+    void agedBrie_qualityIncreasesTwiceAsFastAfterSellIn() {
+        Item[] items = { new Item("Aged Brie", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        
+        app.updateQuality();
+        
+        assertEquals(-1, items[0].sellIn);
+        assertEquals(12, items[0].quality); // Increases by 2
+    }
+
+    @Test
+    void agedBrie_qualityNeverExceeds50() {
+        Item[] items = { new Item("Aged Brie", 5, 50) };
+        GildedRose app = new GildedRose(items);
+        
+        app.updateQuality();
+        
+        assertEquals(4, items[0].sellIn);
+        assertEquals(50, items[0].quality);
+    }
+
 }
